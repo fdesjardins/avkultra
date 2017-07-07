26cdcc53ec1f3b2bffd1a236208ccdbb1419d27d
Forrest Desjardins <desjardinsfg@gmail.com>
2017-07-07 04:34:18 -0800

    refactor state

--- client/state.js
+++ client/state.js
@@ -1,8 +1,8 @@
 import Baobab from 'baobab'
 import superagent from 'superagent'
 import _ from 'lodash'
+import Promise from 'bluebird'
 
-import { incrementCount } from '-/actions/actions'
 import { apiFetch, flightAwareFetch, notamsFetch } from '-/utils'
 
 const state = new Baobab({
@@ -21,70 +21,80 @@ const state = new Baobab({
   }
 })
 
-const locationRequests = _.range(13).map(i => `/locations?bounds=0,-${i * 15}|90,-${i * 15 - 15}`)
-Promise.all(locationRequests.map(lr => apiFetch(lr)))
-  .then(results => [].concat(...results))
-  .then(locations => {
-    // console.log(locations)
-    if (locations && locations.length > 0) {
-      return _.flatten(locations.map(a => {
-        if (a && a.data && a.data.length > 0) {
-          let visibility = 1
-          if (a.display && a.display.length > 0) {
-            // console.log(a.display)
-            const lowest = a.display.find(d => d.markerStyle === 'FULL')
-            if (lowest && lowest.level) {
-              visibility = 13 - lowest.level
-            }
+const fetchLocations = () => {
+  const locationRequests = _.range(13).map(i => `/locations?bounds=0,-${i * 15}|90,${-1 * (i * 15 - 15)}`)
+  return Promise.all(locationRequests.map(lr => apiFetch(lr)))
+    .then(results => [].concat(...results))
+}
+
+const extractAirports = locations => {
+  if (locations && locations.length > 0) {
+    return _.flatten(locations.map(a => {
+      if (a && a.data && a.data.length > 0) {
+        let visibility = 1
+        if (a.display && a.display.length > 0) {
+          const lowest = a.display.find(d => d.markerStyle === 'FULL')
+          if (lowest && lowest.level) {
+            visibility = 13 - lowest.level
           }
-          return a.data.map(d => {
-            if (d.type === 'airport') {
-              return _.merge({}, d, { visibility: Math.pow(visibility, 3.5) })
-            }
-          })
         }
-      })).filter(x => !!x)
-    }
-  })
+        return a.data.map(d => {
+          if (d.type === 'airport') {
+            return _.merge({}, d, { visibility: Math.pow(visibility, 3.5) })
+          }
+        })
+      }
+    })).filter(x => !!x)
+  }
+}
+
+const initAirports = () => fetchLocations()
+  .then(extractAirports)
   .then(airports => {
     state.select('globe', 'airports').set(airports)
   })
 
-apiFetch('/aircraft-reports/pireps')
+const initPireps = () => apiFetch('/aircraft-reports/pireps')
   .then(pireps => state.select('globe', 'aircraftReports').set(pireps))
 
-apiFetch('/aircraft-reports/aireps?bounds=0,-180|90,1')
+const initAireps = () => apiFetch('/aircraft-reports/aireps?bounds=0,-180|90,1')
   .then(aireps => _.uniqBy(aireps, a => a.aircraftRef))
   .then(aireps => {
     state.select('globe', 'aireps').set(aireps)
-    aireps.map(airep => {
-      flightAwareFetch(airep.aircraftRef)
-        .then(aircraftInfo => {
-          state.select('globe', 'aircraft').push(aircraftInfo)
-        })
-    })
   })
 
-apiFetch('/stations?bounds=0,-180|90,1')
+const initAircraft = () => {
+  return state.select('globe', 'aireps').get().map(airep => {
+    return flightAwareFetch(airep.aircraftRef)
+      .then(aircraftInfo => {
+        state.select('globe', 'aircraft').push(aircraftInfo)
+      })
+  })
+}
+
+const initStations = () => apiFetch('/stations?bounds=0,-180|90,1')
   .then(stations => state.select('globe', 'stations').set(stations))
 
-apiFetch('/sites')
-  .then(sites => state.select('globe', 'sites').set(sites))
+const initSites = () => {
+  return apiFetch('/sites')
+    .then(sites => state.select('globe', 'sites').set(sites))
+}
 
-// apiFetch('/navaids')
+// const initNotams = () => apiFetch('/navaids')
 //   .then(navaids => state.select('globe', 'navaids').set(navaids))
 
-notamsFetch('kzdv')
+const initNotams = () => notamsFetch('kzdv')
   .then(notams => state.select('globe', 'notams').set(notams))
 
+// https://stackoverflow.com/a/19356304/3011062
 const calculateNewPosition = (lat, long, bearing, distance) => {
   const R = 6371
-  var lat2 = Math.asin(Math.sin(Math.PI / 180 * lat) * Math.cos(distance / R) + Math.cos(Math.PI / 180 * lat) * Math.sin(distance / R) * Math.cos(Math.PI / 180 * bearing))
-  var lon2 = Math.PI / 180 * long + Math.atan2(Math.sin(Math.PI / 180 * bearing) * Math.sin(distance / R) * Math.cos(Math.PI / 180 * lat), Math.cos(distance / R) - Math.sin(Math.PI / 180 * lat) * Math.sin(lat2))
+  const lat2 = Math.asin(Math.sin(Math.PI / 180 * lat) * Math.cos(distance / R) + Math.cos(Math.PI / 180 * lat) * Math.sin(distance / R) * Math.cos(Math.PI / 180 * bearing))
+  const lon2 = Math.PI / 180 * long + Math.atan2(Math.sin(Math.PI / 180 * bearing) * Math.sin(distance / R) * Math.cos(Math.PI / 180 * lat), Math.cos(distance / R) - Math.sin(Math.PI / 180 * lat) * Math.sin(lat2))
   return [180 / Math.PI * lat2, 180 / Math.PI * lon2]
 }
 
-setInterval(() => {
+const updateAircraftPositions = () => {
   const aircraftCursor = state.select('globe', 'aircraft')
   const updatedAircraft = aircraftCursor.get().map(aircraft => {
     const newPosition = calculateNewPosition(
@@ -99,6 +109,20 @@ setInterval(() => {
     })
   })
   aircraftCursor.set(updatedAircraft)
-}, 500)
+}
+
+const initState = () => {
+  return Promise.map([
+    () => initAireps().then(() => initAircraft()),
+    initPireps,
+    initStations,
+    initNotams,
+    initSites,
+    initAirports
+  ], x => x(), { concurrency: 2 })
+    .then(() => setInterval(updateAircraftPositions, 500))
+}
+
+initState()
 
 export default state
